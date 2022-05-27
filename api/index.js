2359529e8621225455f16d30fd8a0442fbc49da1
ran-pe <itpe.youngran@gmail.com>
2022-05-27 20:17:21 +0900

    프로젝트 소스코드 업로드

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+})
+
+function fetchProductById(id) {
+  return instance.get(`/products/${id}`)
+}
+
+function fetchProductByKeyword(keyword) {
+  return instance.get(`/products`, {
+    params: {
+      name_like: keyword,
+    },
+  })
+}
+
+// carts
+function fetchCartItems() {
+  return instance.get('/carts')
+}
+
+function createCartItem(cartItem) {
+  return instance.post('/carts', cartItem)
+}
+
+export {
+  fetchProductById,
+  fetchProductByKeyword,
+  createCartItem,
+  fetchCartItems,
+}
---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="app">
+    <main>
+      <div>
+        <SearchInput
+          v-model="searchKeyword"
+          @search="searchProducts"
+        ></SearchInput>
+        <!-- <SearchInput
+          :search-keyword="searchKeyword"
+          @input="updateSearchKeyword"
+        ></SearchInput> -->
+      </div>
+      <ul>
+        <li
+          v-for="product in products"
+          :key="product.id"
+          class="item flex"
+          @click="moveToDetailPage(product.id)"
+        >
+          <img
+            class="product-image"
+            :src="product.imageUrl"
+            :alt="product.name"
+          />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+      <div class="cart-wrapper">
+        <button class="btn" @click="moveToCartPage">장바구니 바로가기</button>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import SearchInput from '~/components/SearchInput.vue'
+import { fetchProductByKeyword } from '~/api'
+
+export default {
+  components: {
+    SearchInput,
+  },
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+    // const products = response.data.map((item) => ({
+    //   ...item,
+    //   imageUrl:`${item.imageUrl}?random=${Math.random()}`
+    // }))
+    return { products }
+  },
+  data() {
+    return {
+      searchKeyword: '',
+    }
+  },
+  methods: {
+    moveToDetailPage(id) {
+      this.$router.push(`detail/${id}`)
+    },
+    // updateSearchKeyword(keyword) {
+    //   this.searchKeyword = keyword
+    // },
+    async searchProducts() {
+      const response = await fetchProductByKeyword(this.searchKeyword)
+      this.products = response.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+    },
+    moveToCartPage() {
+      this.$router.push('/cart')
+    }
+  },
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
